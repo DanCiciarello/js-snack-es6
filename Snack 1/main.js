ce756a9535b946b164fd775ec6388c1b654bbbf3
DanCiciarello <me@danieleciciarello.com>
2022-06-06 17:42:07 +0200

    Start project

---
+++ Snack 1/main.js
@@ -0,0 +1,14 @@
+/*
+Richiesta:
+Creare un array di oggetti.
+Ogni oggetto descriverà una bici da corsa con le
+seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando
+destructuring e template literal.
+*/
+
+// --------------------------------------------------------- //
+
+// Recupero gli elementi dal dom
+let resultBike = document.querySelector("#resultBike");
+
