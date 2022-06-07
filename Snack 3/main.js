1d6266bf33d71dacd822d935aca9d81324aba136
DanCiciarello <me@danieleciciarello.com>
2022-06-07 14:32:29 +0200

    Completed Snack 3

--- Snack 3/main.js
+++ Snack 3/main.js
@@ -9,3 +9,52 @@ Eseguiamo questo esercizio prima con forEach e poi con filter.
 */
 
 // --------------------------------------------------------- //
+
+// Creo l'array
+const myArray = [
+    "Michele",
+    "Fabio",
+    "Roberto",
+    "Giovanni",
+    "Simone",
+    "Chiara"
+];
+
+// Creo i valori min e max
+const numMin = 2;
+const numMax = 4;
+
+// Creo un array vuoto
+let arrayForEach = [];
+
+// Creo un nuovo array con forEach
+myArray.forEach((element, i) => {
+
+    // Controllo se l'elemento attuale è compreso tra min e max
+    if (i >= numMin && i <= numMax) {
+
+        // Pusho l'elemento nel nuovo array se compreso
+        arrayForEach.push(element);
+
+    };
+
+});
+
+console.log("------ Array forEach ------");
+console.log(arrayForEach);
+
+// --------------------------------------------------------- //
+
+// Creo un nuovo array con Filter
+const arrayFilter = myArray.filter((element, i) => {
+
+    if (i >= numMin && i <= numMax) {
+        return true;
+    } else {
+        return false;
+    };
+
+});
+
+console.log("------ Array Filter ------");
+console.log(arrayFilter);
